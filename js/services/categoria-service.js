66bd64b69d7daaba66f5bcce2326f5284f76a25b
Bel Cogo <bel.cogo@cwi.com.br>
2017-11-04 17:53:35 -0200

    fix(Inspire): corrigindo nomes dos métodos da service de categoria.

--- js/services/categoria-service.js
+++ js/services/categoria-service.js
@@ -1,12 +1,12 @@
 app.factory('categoriaService', function ($http) {
     let urlBase = "http://localhost:3000/categoria";
-    function criarcategoria(categoria){
+    function criarCategoria(categoria){
         return $http.post(urlBase, categoria);
     }
-    function editarcategoria(categoria){
+    function editarCategoria(categoria){
         return $http.put(urlBase + `/${categoria.id}`, categoria);
     }
-    function deletarcategoria(categoria){
+    function deletarCategoria(categoria){
         return $http.delete(urlBase + `/${categoria.id}`);
     }
     function buscarPorId(categoria){
@@ -16,9 +16,9 @@ app.factory('categoriaService', function ($http) {
         return $http.get(urlBase);
     }
     return {
-        criarcategoria : criarcategoria,
-        editarcategoria : editarcategoria,
-        deletarcategoria : deletarcategoria,
+        criarCategoria : criarCategoria,
+        editarCategoria : editarCategoria,
+        deletarCategoria : deletarCategoria,
         buscarPorId : buscarPorId,
         listar : listar
     }
