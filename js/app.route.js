e280062edf54e62a780a99e9b9fa366be298f7ef
Bel Cogo <bel.cogo@cwi.com.br>
2017-11-04 17:49:49 -0200

    wip(Inspire): implementando rotas do sistema.

---
+++ js/app.route.js
@@ -0,0 +1,24 @@
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/usuario', {
+    templateUrl: 'usuario/index.html',
+    controller: 'usuarioCtrl'
+  })
+  .when('/ideia', {
+    templateUrl: 'ideia/index.html',
+    controller: 'ideiaCtrl'
+  })
+  .when('/feed', {
+    templateUrl: 'ideia/feed.html',
+    controller: 'feedCtrl'
+  })
+  .when('/usuario-detail', {
+    templateUrl: 'usuario/detail.html',
+    controller: 'usuarioDetailCtrl'
+  })
+  .when('/ideia-detail', {
+    templateUrl: 'ideia/detail.html',
+    controller: 'ideiaDetailCtrl'
+  })
+  .otherwise('/feed');
+});
